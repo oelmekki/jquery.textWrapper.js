781ed2b3173ad29d7ee4c1888d12f04614cdcfb2
Olivier El Mekki <olivier@el-mekki.com>
2013-07-30 11:30:19 +0200

    ADD demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+$(function(){
+  var $demo, $textarea, $on, $off, showWrapState;
+
+  $demo = $( '#demo-container' );
+  $textarea = $demo.find( 'textarea' );
+  $on = $demo.find( '.on' );
+  $off = $demo.find( '.off' );
+
+  showWrapState = function( event, autowrap ){
+    if ( autowrap ){
+      $on.show();
+      $off.hide();
+    }
+
+    else {
+      $off.show();
+      $on.hide();
+    }
+  }
+
+  $textarea.on( 'toggleAutoWrap', showWrapState );
+  $textarea.textWrapper();
+});
